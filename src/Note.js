08b46b2362ba3f846f28a714ebd90a138945c0d9
Dylan Wilkinson <dwilkinson167@southseattle.edu>
2018-01-29 21:36:50 -0800

    Save and Edit state added

--- src/Note.js
+++ src/Note.js
@@ -1,30 +1,63 @@
 import React, { Component } from 'react'
 import FaPencil from 'react-icons/lib/fa/pencil'
 import FaTrash from 'react-icons/lib/fa/trash'
+import FaFloppyO from 'react-icons/lib/fa/floppy-o'
 
 class Note extends Component {
     constructor(props) {
         super(props);
-        Note.edit = Note.edit.bind(this);
-        Note.remove = Note.remove.bind(this)
+        this.state = {
+            editing: false
+        };
+        this.edit = this.edit.bind(this);
+        this.remove = this.remove.bind(this);
+        this.save = this.save.bind(this);
+        this.renderForm = this.renderForm.bind(this);
+        this.renderDisplay = this.renderDisplay.bind(this);
     }
-    static edit() {
-        alert('editing note')
+
+    edit() {
+        this.setState({
+            editing: true
+        })
     }
-    static remove() {
+
+    remove() {
         alert('removing note')
     }
-    render() {
+
+    save() {
+        alert('saved!')
+    }
+
+    renderForm() {
+        return (
+            <div className="note">
+                <form>
+                    <textarea/>
+                    <button onClick={this.save}><FaFloppyO/></button>
+                </form>
+            </div>
+        )
+    }
+
+    renderDisplay() {
         return (
             <div className="note">
                 <p>Learn React</p>
                 <span>
-                    <button onClick={Note.edit} id="edit"><FaPencil/></button>
-                    <button onClick={Note.remove} id="remove"><FaTrash/></button>
-                </span>
+        <button onClick={this.edit} id="edit"><FaPencil/></button>
+        <button onClick={this.remove} id="remove"><FaTrash/></button>
+        </span>
             </div>
 
         )
+    }
+
+    render() {
+
+        return this.state.editing ? this.renderForm() : this.renderDisplay()
+        
     }
 }
 

bbf1c6364b20101eed08735a03e6b31211d7d86e
Dylan Wilkinson <dwilkinson167@southseattle.edu>
2018-01-29 21:11:31 -0800

    Added constructor and alerts for remove and edit button

--- src/Note.js
+++ src/Note.js
@@ -3,13 +3,24 @@ import FaPencil from 'react-icons/lib/fa/pencil'
 import FaTrash from 'react-icons/lib/fa/trash'
 
 class Note extends Component {
+    constructor(props) {
+        super(props);
+        Note.edit = Note.edit.bind(this);
+        Note.remove = Note.remove.bind(this)
+    }
+    static edit() {
+        alert('editing note')
+    }
+    static remove() {
+        alert('removing note')
+    }
     render() {
         return (
             <div className="note">
                 <p>Learn React</p>
                 <span>
-                    <button id="edit"><FaPencil/></button>
-                    <button id="remove"><FaTrash/></button>
+                    <button onClick={Note.edit} id="edit"><FaPencil/></button>
+                    <button onClick={Note.remove} id="remove"><FaTrash/></button>
                 </span>
             </div>
 
